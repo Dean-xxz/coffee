07d5d4d6e460c900706bdf92c7dd10abe305a4f8
Dean-xxz <1220543004@qq.com>
2018-03-13 20:48:28 +0800

    添加创建提货码接口

--- access_code/apis.py
+++ access_code/apis.py
@@ -8,7 +8,7 @@
 from utils.view_tools import ok_json, fail_json,get_args
 from utils.abstract_api import AbstractAPI
 
-from .models import Formula,Container
+from .models import Access_Code
 
 # 取货码创建接口
 
@@ -16,9 +16,13 @@ class CodeCreateAPI(AbstractAPI):
     def config_args(self):
         self.args = {
             'item_id':'r',
+            'user_id':('o',None),
         }
 
     def access_db(self, kwarg):
+        item_id = kwarg['item_id']
+        user_id = kwarg['user_id']
+        code = ''.join(random.sample(['a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4','5','6','7','8','9','0'],6)).replace("", "")
 
 
     def format_data(self, data):
