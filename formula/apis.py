a3533de3a0d8003679a46c6788a66d5a44326b51
root <root@1220543004@qq.com>
2018-04-11 10:23:31 +0800

    修改配方查询接口

--- formula/apis.py
+++ formula/apis.py
@@ -16,32 +16,58 @@ from product.models import Item
 class FormulaQueryAPI(AbstractAPI):
     def config_args(self):
         self.args = {
-            'item_id':'r',
+            'item_id':('o',None),
         }
 
     def access_db(self, kwarg):
         item_id = kwarg['item_id']
 
-        formula = Formula.objects.filter(item_id=item_id,is_active=True).order_by('order')
-        data = [o.get_json() for o in formula]
+        if item_id:
+            formula = Formula.objects.filter(item_id=item_id,is_active=True).order_by('order')
+            data = [o.get_json() for o in formula]
+            for i in data:
+                container_id = i['container']
+                container = Container.objects.get(pk=container_id)
+                container_name = container.title
+                container_id = container.order
+                i['container_id'] = container_id
+                item = Item.objects.get(pk=item_id)
+                i['hotcoolchoice'] = item.can_select
+                item_title = item.title
+                i['item_title'] = item_title
+                i['container_name'] = container_name
+                print (type(i['effluentinterval']))
+                i['effluentinterval'] = float(i['effluentinterval'])
+                i.pop('create_time')
+                i.pop('update_time')
+                i.pop('is_active')
+                i.pop('remarks')
+                i.pop('id')
+            return data
+        item_list = Item.objects.filter(is_active=True)
+        data = [o.get_json() for o in item_list]
         for i in data:
-            container_id = i['container']
-            container = Container.objects.get(pk=container_id)
-            container_name = container.title
-            container_id = container.order
-            i['container_id'] = container_id
-            item = Item.objects.get(pk=item_id)
-            i['hotcoolchoice'] = item.can_select
-            item_title = item.title
-            i['item_title'] = item_title
-            i['container_name'] = container_name
-            print (type(i['effluentinterval']))
-            i['effluentinterval'] = float(i['effluentinterval'])
             i.pop('create_time')
             i.pop('update_time')
             i.pop('is_active')
-            i.pop('remarks')
-            i.pop('id')
+            i.pop('category')
+            item_id = i['id']
+            formula = Formula.objects.filter(item_id=item_id,is_active=True).order_by('order')
+            formula_list = [o.get_json() for o in formula]
+            for j in formula_list:
+                container_id = j['container']
+                container = Container.objects.get(pk=container_id)
+                container_name = container.title
+                container_id = container.order
+                j['container_id'] = container_id
+                j['container_name'] = container_name
+                j['effluentinterval'] = float(j['effluentinterval'])
+                j.pop('create_time')
+                j.pop('update_time')
+                j.pop('is_active')
+                j.pop('remarks')
+                j.pop('id')
+            i['formula_detail'] = formula_list
         return data
 
     def format_data(self, data):
